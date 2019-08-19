17051e270303d7b7826f7919098b533e8b2dfd60
haschish <haschish@gmail.com>
2019-08-20 00:17:13 +0500

    module4-task1: fixed mentor's comments 2

--- schema.sql
+++ schema.sql
@@ -5,25 +5,23 @@ CREATE DATABASE IF NOT EXISTS 63289_doingsdone_10
 USE 63289_doingsdone_10;
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `name` varchar(64) NOT NULL DEFAULT '',
   `email` varchar(64) NOT NULL DEFAULT '',
   `password` varchar(64) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `projects` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `projects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id` int(11) unsigned NOT NULL,
   `name` varchar(256) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id`),
   UNIQUE KEY `name_unique` (`user_id`,`name`),
   CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `tasks` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `user_id` int(11) unsigned NOT NULL,
   `project_id` int(11) unsigned DEFAULT NULL,
   `name` varchar(256) NOT NULL DEFAULT '',
@@ -31,7 +29,8 @@ CREATE TABLE `tasks` (
   `date` date NOT NULL,
   `done` tinyint(1) NOT NULL DEFAULT '0',
   `dt_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+  KEY `project_id` (`project_id`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tasks_ibfk_2` FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
